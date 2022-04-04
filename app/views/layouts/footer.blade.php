f9326366faee64e640640ccfcdc897dc74688515
Chou <chouh@fsabcl-onc01d.ncifcrf.gov>
2022-04-04 17:30:39 -0400

    footer url fixed

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -3,10 +3,10 @@
     <div class="site-footer__container">
         <div class="site-footer__agencies">
             <ul class="top_footer_creds">
-                <li><a rel="nofollow noopener noreferrer" class="nci_footer_logo" href="https://www.cancer.gov/" target="_blank" title="Go to NCI Homepage"><img alt="NCI logo" src="images/nci-logo-full-white.svg"></a></li>
+                <li><a rel="nofollow noopener noreferrer" class="nci_footer_logo" href="https://www.cancer.gov/" target="_blank" title="Go to NCI Homepage"><img alt="NCI logo" src="{{url()}}/images/nci-logo-full-white.svg"></a></li>
                 <li><a target=blank href="http://home.ccr.cancer.gov/oncology/oncogenomics/">Khan Lab Home</a></li>
                 <li><a rel="nofollow noopener noreferrer" href="https://frederick.cancer.gov/research/biomedical-informatics-and-data-science-directorate/advanced-biomedical-computational-science" target="_blank">Advanced Biomedical Computational Science</a></li>
-                <li><a rel="nofollow noopener noreferrer" class="fnlcr_footer_logo" href="https://frederick.cancer.gov/" target="_blank" title="Go to FNLCR Homepage"><img alt="FNLCR logo" src="images/fnlcr-logo-white.svg"></a></li>
+                <li><a rel="nofollow noopener noreferrer" class="fnlcr_footer_logo" href="https://frederick.cancer.gov/" target="_blank" title="Go to FNLCR Homepage"><img alt="FNLCR logo" src="{{url()}}/images/fnlcr-logo-white.svg"></a></li>
             </ul>
             <ul>
                 <li><a target="_blank" rel="nofollow noopener noreferrer" href="https://www.cancer.gov/policies/disclaimer">Disclaimer Policy</a></li>
