408cfb395363fb657cc9c3c30546e32da5ee41ae
Chou <chouh@fsabcl-onc01d.ncifcrf.gov>
2022-08-25 14:22:04 -0400

    fixed slurm path

--- app/scripts/slurm/submitPreprocessProject.sh
+++ app/scripts/slurm/submitPreprocessProject.sh
@@ -5,4 +5,4 @@
 #SBATCH --time=24:00:00
 
 #example: sbatch /mnt/projects/CCR-JK-oncogenomics/static/site_data/prod/submitPreprocessProject.sh /mnt/projects/CCR-JK-oncogenomics/static/ProcessedResults/update_list/compass_exome_db_20220405-233934_caselist.txt chouh@nih.gov https://oncogenomics.ccr.cancer.gov/production/public
-${SLURM_SUBMIT_DIR}/../preprocessProjectMaster.pl -p $1 -e $2 -u $3 -m -g
+$PWD/../preprocessProjectMaster.pl -p $1 -e $2 -u $3 -m -g
